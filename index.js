9201edaa0b20882f2bd0d4ab3192a2d90e8592fc
takaiyuki-oss <takai_yuki@cyberagent.co.jp>
2025-10-08 12:28:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ async function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest"});
+  try {const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
     const result = await model.generateContent(event.message.text);
     const response = await result.response;
     const aiReply = response.text();
